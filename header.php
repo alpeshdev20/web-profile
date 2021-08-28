2fa1c5983591b197574dbe8c1a6fe8ab04b10440
Alpesh Vaghela <alpesh.dev20@gmail.com>
2021-08-28 17:58:13 +0530

    Add email funtionality

--- header.php
+++ header.php
@@ -5,12 +5,12 @@
       </div>
       <nav class="nav-menu d-none d-lg-block">
         <ul>
-          <li class="active"><a href="index.php#header">Home</a></li>
-          <li><a href="index.php#about">About Us</a></li>
-          <li><a href="index.php#services">Services</a></li>
-          <li><a href="index.php#portfolio">Portfolio</a></li>
-          <li><a href="index.php#contact">Contact Us</a></li>
-          <li class="get-started"><a href="request-a-quote.php">Request a quote</a></li>
+          <li class="active"><a href="#header">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#services">Services</a></li>
+          <li><a href="#portfolio">Portfolio</a></li>
+          <li><a href="#contact">Contact Us</a></li>
+          <!-- <li class="get-started"><a href="request-a-quote.php">Request a quote</a></li> -->
         </ul>
       </nav>
     </div>
